427bc3d6b53d38312d8fa08d67d845bcc2112a94
leonardo.monteiro <hsd.leo@gmail.com>
2016-08-04 09:39:42 -0300

    metodos e classes triangulos

--- Saudacao.java
+++ Saudacao.java
@@ -4,9 +4,37 @@ import java.util.Scanner;
 class Saudacao{
 		public static void main(String args[]){
 						
-			String nome = JOptionPane.showInputDialog("Digite seu nome");
-			
-			System.out.println("Digitou " + nome);
-			
+			double altura = Double.parseDouble(JOptionPane.showInputDialog("Digite a altura do Triangulo :"));
+			double base = Double.parseDouble(JOptionPane.showInputDialog("Digite a base do Triangulo :"));
+
+			Triangulo t1 = new Triangulo(altura,base);
+			System.out.println("A área do triângulo é : " + areaTriangulo(base,altura));
+						System.out.println("A área do triângulo2 é : " + t1.Area());
+
+		}
+		
+		public static double areaTriangulo(double base,double altura){
+			return (base*altura/2);
+		}
+		public static double areaRetangulo(double base,double altura){
+			return (base*altura);
 		}
 }
+
+
+class Triangulo{
+	double altura;
+	double base;
+	
+	public Triangulo(double a,double b){
+		this.altura = a;
+		this.base = b;
+	}
+	
+	public double Area(){
+		return ((altura*base)/2);
+	}
+	
+	
+}
+
