67bc7b402410335652b73904efd5760fa72dc106
leonardo.monteiro <hsd.leo@gmail.com>
2016-08-04 09:07:25 -0300

    Input JOptionPane

--- Saudacao.java
+++ Saudacao.java
@@ -1,6 +1,12 @@
+import javax.swing.JOptionPane;
+import java.util.Scanner;
+
 class Saudacao{
 		public static void main(String args[]){
+						
+			String nome = JOptionPane.showInputDialog("Digite seu nome");
 			
-			System.out.println("Uhhuulll  " + args[0]);
+			System.out.println("Digitou " + nome);
+			
 		}
 }
